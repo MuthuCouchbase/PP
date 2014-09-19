8f6e59ad6b2f5df0b094d07c4583dc81b7a0d87b
Muthukumar Koteeswaran <mkumar@couchbase.com>
2014-09-19 19:14:17 +0530

    Update Jenkins_Console_Parser.pl

--- Jenkins_Console_Parser.pl
+++ Jenkins_Console_Parser.pl
@@ -1,26 +1,27 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-my ($total,$failures,$count);
+my ($total,$failures);
 my $start = $ARGV[0];
 my $end = $ARGV[1];
 my $job = $ARGV[2];
+print "JobName Total_Runs Total_Failures SUCCESS_PERCENTAGE\n";
 if(!$start || !$end || !$job) {
 die "Provide start iteration number, end iteration number and jobName space delimited\n";
 }
 my $DIR = "/x/hudson/jobs/$job/builds";
 for(my $i = $start; $i<=$end; $i++) {
+if( -d $DIR) {
 `strings "$DIR/$i/log" > /var/tmp/abcd`;
 my $output = `grep 'Tests run: ' /var/tmp/abcd| grep -v '^Test'`;
 `rm /var/tmp/abcd`;
 chomp($output);
 if($output =~ /Tests run: (\d+), Failures: (\d+)/) {
-$total+=1;
-$failures+=1;
-print "$i,$1,$2\n";
+$total+=$1;
+$failures+=$2;
 }
-$count+=1;
 }
-my $percentage = (($failures/$total)/$count)*100;
-print "$job $total $failures $percentage \n";
+}
+my $percentage = (($total-$failures)/$total)*100;
+print "$job $total $failures $percentage"."%\n";
 
