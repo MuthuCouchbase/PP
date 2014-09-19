6c7232352d23a7e19576c665700681c75b9a5d45
Muthukumar Koteeswaran <mkumar@couchbase.com>
2014-09-19 18:58:58 +0530

    Update Jenkins_Console_Parser.pl

--- Jenkins_Console_Parser.pl
+++ Jenkins_Console_Parser.pl
@@ -9,7 +9,9 @@ die "Provide start iteration number, end iteration number and jobName space deli
 }
 my $DIR = "/x/hudson/jobs/$job/builds";
 for(my $i = $start; $i<=$end; $i++) {
-my $output = `grep 'Tests run: ' "$DIR/$i/log"| grep -v 'Time elapsed'`;
+`strings "$DIR/$i/log" > /var/tmp/abcd`;
+my $output = `grep 'Tests run: ' /var/tmp/abcd| grep -v '^Test'`;
+`rm /var/tmp/abcd`;
 chomp($output);
 if($output =~ /Tests run: (\d+), Failures: (\d+)/) {
 print "$i,$1,$2\n";
