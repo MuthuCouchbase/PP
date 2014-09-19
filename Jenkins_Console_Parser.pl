ac9d795fdab91d8e0f412b902d873fb4e72cca89
Muthukumar Koteeswaran <mkumar@couchbase.com>
2014-09-19 19:06:37 +0530

    Update Jenkins_Console_Parser.pl

--- Jenkins_Console_Parser.pl
+++ Jenkins_Console_Parser.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+my ($total,$failures,$count);
 my $start = $ARGV[0];
 my $end = $ARGV[1];
 my $job = $ARGV[2];
@@ -14,6 +15,12 @@ my $output = `grep 'Tests run: ' /var/tmp/abcd| grep -v '^Test'`;
 `rm /var/tmp/abcd`;
 chomp($output);
 if($output =~ /Tests run: (\d+), Failures: (\d+)/) {
+$total+=1;
+$failures+=1;
 print "$i,$1,$2\n";
 }
+$count+=1;
 }
+my $percentage = (($failures/$total)/$count)*100;
+print "$job $total $failures $percentage \n";
+
